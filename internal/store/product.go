0a7113377727f29fc29fe381b1f0cca344ef4f44
Vialmsi <denis.leonov@innowise-group.com>
2023-07-05 18:12:36 +0300

    ServiceLayer

--- internal/store/product.go
+++ internal/store/product.go
@@ -2,33 +2,49 @@ package store
 
 import (
 	"encoding/json"
+	"errors"
+	"fmt"
 	"os"
 	"path/filepath"
+	"strconv"
+	"strings"
 
 	"github.com/Vialmsi/Interview/internal/entity"
 )
 
 const (
-	FileStorage    = "files"
-	FilePermission = 0644
-	JSONExtension  = ".json"
+	fileStorage   = "files"
+	jsonExtension = ".json"
+	// filePermission - readable by all the user groups, but writable by the user only
+	filePermission = 0644
+	// dirPermission - full permissions
+	dirPermission = 0777
 )
 
 type ProductStore struct {
 }
 
-func NewProductStore() *ProductStore {
-	return &ProductStore{}
+func NewProductStore() (*ProductStore, error) {
+	_, err := os.Open(fileStorage)
+	if errors.Is(err, os.ErrNotExist) {
+		err := os.Mkdir(fileStorage, dirPermission)
+		if err != nil {
+			return nil, fmt.Errorf("error while creating file storage: %s", err.Error())
+		}
+	}
+	return &ProductStore{}, nil
 }
 
 // SaveProduct func which allows to save product into dir
-func (p *ProductStore) SaveProduct(fileName, userDir string, product entity.Product) error {
+func (p *ProductStore) SaveProduct(product entity.Product) error {
 	data, err := json.Marshal(product)
 	if err != nil {
 		return err
 	}
 
-	err = os.WriteFile(filepath.Join(FileStorage, userDir, fileName)+JSONExtension, data, FilePermission)
+	strID := strconv.Itoa(product.UserID)
+
+	err = os.WriteFile(filepath.Join(fileStorage, strID, product.Barcode)+jsonExtension, data, filePermission)
 	if err != nil {
 		return err
 	}
@@ -37,8 +53,9 @@ func (p *ProductStore) SaveProduct(fileName, userDir string, product entity.Prod
 }
 
 // RetrieveProduct func which allows to get product from dir using file name
-func (p *ProductStore) RetrieveProduct(fileName string, userDir string) (*entity.Product, error) {
-	f, err := os.Open(filepath.Join(FileStorage, userDir, fileName))
+func (p *ProductStore) RetrieveProduct(fileName string, userID int) (*entity.Product, error) {
+	strID := strconv.Itoa(userID)
+	f, err := os.Open(filepath.Join(fileStorage, strID, fileName+jsonExtension))
 	if err != nil {
 		return nil, err
 	}
@@ -66,13 +83,15 @@ func (p *ProductStore) RetrieveProduct(fileName string, userDir string) (*entity
 }
 
 // DeleteProduct func which allows to delete product from dir
-func (p *ProductStore) DeleteProduct(fileName string, userDir string) error {
-	return os.Remove(filepath.Join(FileStorage, userDir, fileName) + JSONExtension)
+func (p *ProductStore) DeleteProduct(fileName string, userID int) error {
+	strID := strconv.Itoa(userID)
+	return os.Remove(filepath.Join(fileStorage, strID, fileName) + jsonExtension)
 }
 
 // RetrieveProductsByUserID func which allows to get every user's product
-func (p *ProductStore) RetrieveProductsByUserID(userDir string) ([]entity.Product, error) {
-	dir, err := os.Open(filepath.Join(FileStorage, userDir))
+func (p *ProductStore) RetrieveProductsByUserID(userID int) ([]entity.Product, error) {
+	strID := strconv.Itoa(userID)
+	dir, err := os.Open(filepath.Join(fileStorage, strID))
 	if err != nil {
 		return nil, err
 	}
@@ -86,7 +105,7 @@ func (p *ProductStore) RetrieveProductsByUserID(userDir string) ([]entity.Produc
 	products := make([]entity.Product, len(files))
 
 	for i, file := range files {
-		product, err := p.RetrieveProduct(file.Name(), userDir)
+		product, err := p.RetrieveProduct(strings.Trim(file.Name(), jsonExtension), userID)
 		if err != nil {
 			return nil, err
 		}
@@ -94,4 +113,10 @@ func (p *ProductStore) RetrieveProductsByUserID(userDir string) ([]entity.Produc
 	}
 
 	return products, nil
+}
+
+func (p *ProductStore) CreateUserStorage(userID int) error {
+	strID := strconv.Itoa(userID)
+
+	return os.Mkdir(filepath.Join(fileStorage, strID), dirPermission)
 }
