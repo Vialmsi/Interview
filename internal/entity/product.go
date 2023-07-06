86469e27348dcd7c0f390440528157b48fc3ccee
Vialmsi <138578333+Vialmsi@users.noreply.github.com>
2023-07-06 15:10:58 +0300

    Revert "Handler layer"

--- internal/entity/product.go
+++
@@ -1,9 +0,0 @@
-package entity
-
-type Product struct {
-	Barcode string `json:"barcode"`
-	Name    string `json:"name"`
-	Desc    string `json:"desc"`
-	Cost    int    `json:"cost"`
-	UserID  int    `json:"user_id"`
-}
